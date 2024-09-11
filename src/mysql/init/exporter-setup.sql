109c6c1a6c0ebc8230ad4f6df8c2ddf4c72932fc
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2024-09-10 18:50:14 -0700

    Update exporter-setup.sql

--- src/mysql/init/exporter-setup.sql
+++ src/mysql/init/exporter-setup.sql
@@ -1,3 +1,5 @@
 CREATE USER 'exporter'@'%' IDENTIFIED BY 'wiley123';
 GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'exporter'@'%';
 GRANT SELECT ON *.* TO 'exporter'@'%';
+CREATE USER 'replicator'@'%' IDENTIFIED BY 'wiley123';
+GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';
