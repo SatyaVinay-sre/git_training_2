0402d3004c5b0909d677b5316b645ed64f6997d1
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2024-09-17 09:43:33 -0700

    Update SQLClasses.py

--- src/fastapi/app/SQLClasses.py
+++ src/fastapi/app/SQLClasses.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, DECIMAL
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, DECIMAL, Table
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
