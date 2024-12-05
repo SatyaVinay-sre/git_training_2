81516c1e3563f9bfec21e420628cf4c3574768a2
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2024-12-05 10:30:20 -0700

    Update roles.sql

--- src/mysql/init/roles.sql
+++ src/mysql/init/roles.sql
@@ -1,3 +1,10 @@
 INSERT INTO Role (name) VALUES ('admin');
 INSERT INTO Role (name) VALUES ('it');
 INSERT INTO Role (name) VALUES ('user');
+
+INSERT INTO User (uname, password, dateJoined) 
+VALUES ('admin', 'abc', CURRENT_TIMESTAMP);
+
+INSERT INTO User (uname, password, dateJoined) 
+VALUES ('wiley', 'abc', CURRENT_TIMESTAMP);
+
