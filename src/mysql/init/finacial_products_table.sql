c2c2a3ed3acd8c2a9b32fbda1fadf7ec50c765d0
dan-wiley <119693510+dan-wiley@users.noreply.github.com>
2024-12-06 10:34:48 -0700

    remove drop table

--- src/mysql/init/finacial_products_table.sql
+++ src/mysql/init/finacial_products_table.sql
@@ -19,18 +19,6 @@
 -- Table structure for table `Product`
 --
 
-DROP TABLE IF EXISTS `Product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Product` (
-  `symbol` varchar(16) NOT NULL,
-  `price` decimal(15,2) DEFAULT NULL,
-  `productType` varchar(12) DEFAULT NULL,
-  `name` varchar(128) DEFAULT NULL,
-  `lastUpdate` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
 --
 -- Dumping data for table `Product`
 --
